be4b505a0d27a07b3234d29e7fb9066bd73d640f
wangli <943542103@qq.com>
2017-04-10 16:08:40 +0800

    first commit

---
+++ FMDB_news/FMDB_news/Model/DynamicModel.m
@@ -0,0 +1,13 @@
+//
+//  DynamicModel.m
+//  水云天
+//
+//  Created by wleleven on 16/5/24.
+//  Copyright © 2016年 Glory. All rights reserved.
+//
+
+#import "DynamicModel.h"
+
+@implementation DynamicModel
+
+@end
---
+++ FMDB_news/FMDB_news/NewsDetailController.h
@@ -0,0 +1,15 @@
+//
+//  NewsDetailController.h
+//  水云天
+//
+//  Created by wleleven on 16/6/23.
+//  Copyright © 2016年 Glory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsDetailController : UIViewController
+
+@property (nonatomic,strong)NSString *newsUrl;
+
+@end
